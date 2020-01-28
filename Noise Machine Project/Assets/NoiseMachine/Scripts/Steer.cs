50e8dd287777448495f4e5f5fd4539e0e99bbd4e
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2020-01-28 16:57:35 -0500

    start of character controller
    added testing scene with a basic character contoller

---
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steer : MonoBehaviour
+{
+    public GameObject board;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //testing with keyboard
+       if (Input.GetAxis("Horizontal") != 0)
+        {
+            //turns based on positive or negitive button press (right or left)
+            board.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 45 * ((Time.deltaTime * Input.GetAxis("Horizontal")) * 50), transform.localEulerAngles.z);
+        }
+    }
+}
