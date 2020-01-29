bd7db29fde11dea48fd3c3b04b46b1e324ac8bcd
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2020-01-29 14:20:44 -0500

    branched off input based motion
    can ski down the hill with changing direction / removed frictionless surface from gravity based branch

--- Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
@@ -5,14 +5,26 @@ using UnityEngine;
 public class Steer : MonoBehaviour
 {
     public GameObject board;
+    public GameObject player;
     // Update is called once per frame
     void FixedUpdate()
     {
+        Turn();
         //testing with keyboard
-       if (Input.GetAxis("Horizontal") != 0)
+       if (Input.GetAxis("Vertical") > 0)
+        {
+            //turns based on positive or negitive button press (right or left)
+            player.transform.position += transform.forward * (Time.deltaTime * Input.GetAxis("Vertical") * 20);
+        }
+    }
+
+    void Turn()
+    {
+        if (Input.GetAxis("Horizontal") != 0)
         {
             //turns based on positive or negitive button press (right or left)
             board.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 45 * ((Time.deltaTime * Input.GetAxis("Horizontal")) * 50), transform.localEulerAngles.z);
         }
     }
+
 }
