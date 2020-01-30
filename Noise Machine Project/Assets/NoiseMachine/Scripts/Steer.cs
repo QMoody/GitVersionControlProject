eac22e6c882030e07e84edb09da691681ead20fb
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2020-01-30 17:26:55 -0500

    Changed the steering system
    Steering is based on mouse position on screen

--- Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
@@ -9,10 +9,25 @@ public class Steer : MonoBehaviour
     void FixedUpdate()
     {
         //testing with keyboard
+<<<<<<< Updated upstream
        if (Input.GetAxis("Horizontal") != 0)
         {
             //turns based on positive or negitive button press (right or left)
             board.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 45 * ((Time.deltaTime * Input.GetAxis("Horizontal")) * 50), transform.localEulerAngles.z);
         }
+=======
+       if (Input.GetAxis("Vertical") > 0)
+        {
+            //turns based on positive or negitive button press (right or left)
+            player.transform.position += transform.forward * (Time.deltaTime * Input.GetAxis("Vertical") * 20);
+        }
+    }
+
+    void Turn()
+    {
+            //turns based on mouse position on screen
+            board.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, (Mathf.Atan2(Input.mousePosition.x - Screen.width/2, Screen.height)*180/Mathf.PI), transform.localEulerAngles.z);
+        
+>>>>>>> Stashed changes
     }
 }
