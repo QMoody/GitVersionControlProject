7bc96b52f0dc4b1d86f473c6973f5c666f975746
yaniwang <59710060+yaniwang@users.noreply.github.com>
2020-01-30 16:59:31 -0500

    Added test script -attempt at conflict #4

---
+++ Noise Machine Project/Assets/Yani's Scripts/garbo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class garbo : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        //hi 
+        //hello
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
