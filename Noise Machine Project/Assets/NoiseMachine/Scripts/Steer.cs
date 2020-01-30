1ce4b569873053cc99aeff53320eae93ca155048
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2020-01-30 17:34:34 -0500

    Gravity is controlled by force
    Board falls in direction board is facing

--- Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/Steer.cs
@@ -6,16 +6,13 @@ public class Steer : MonoBehaviour
 {
     public GameObject board;
     public GameObject player;
+
+    public float gravity;
     // Update is called once per frame
     void FixedUpdate()
     {
         Turn();
-        //testing with keyboard
-       if (Input.GetAxis("Vertical") > 0)
-        {
-            //turns based on positive or negitive button press (right or left)
-            player.transform.position += transform.forward * (Time.deltaTime * Input.GetAxis("Vertical") * 20);
-        }
+        Fall();
     }
 
     void Turn()
@@ -25,6 +22,12 @@ public class Steer : MonoBehaviour
             //turns based on positive or negitive button press (right or left)
             board.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, 45 * ((Time.deltaTime * Input.GetAxis("Horizontal")) * 50), transform.localEulerAngles.z);
         }
+    }
+
+    void Fall()
+    {
+        //falls down the mountain in the direction the board is facing
+        player.transform.position += transform.forward * (Time.deltaTime * gravity);
     }
 
 }
