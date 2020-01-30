8bb9a80e49b8d830b9a3e32caf52906b807ee783
yaniwang <59710060+yaniwang@users.noreply.github.com>
2020-01-30 17:07:51 -0500

    Changed test_script to add goodbye line

---
+++ Noise Machine Project/Assets/test_script.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_script : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+        //hello
+
+        //goodbye
+
+    void Start()
+    {
+        
+    }
+
