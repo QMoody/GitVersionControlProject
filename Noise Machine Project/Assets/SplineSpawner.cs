6f190fbdf1bc992481ed29cb40dd9d5385d6b0b5
Shane-sama <48694586+ShaneLogtenberg@users.noreply.github.com>
2020-02-17 10:25:49 -0500

    Edit SplineSpawner.cs

--- Noise Machine Project/Assets/SplineSpawner.cs
+++ Noise Machine Project/Assets/SplineSpawner.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SplineSpawner : MonoBehaviour
 {
     public BezierSpline spline;
-    public GameObject StartPoint;
+    public GameObject startPoint;
     public Transform leftMarker;
     public Transform rightMarker;
     public float xSpacing;
@@ -15,29 +15,40 @@ public class SplineSpawner : MonoBehaviour
 
     private void Start()
     {
-        for(int p = 0; p < spline.ControlPointCount; p+=3)
-        {
-            DropControlPoint(p);
+        left = leftMarker.position.x;
+        right = rightMarker.position.x;
+        spline.SetControlPoint(0, DropPoint(startPoint.transform.position));
+        for (int p = 3; p < spline.ControlPointCount; p+=3)
+        {                 
+            spline.SetControlPoint(p, DropPoint(randomPoint(p))); //Drops point right on the surrface of that collider
         }
         for (int p = 1; p < spline.ControlPointCount; p += 3)
         {
-            DropControlPoint(p);
+            spline.SetControlPoint(p, DropPoint(randomPoint(p))); //Drops point right on the surrface of that collider
+        }
+        for (int p = 2; p < spline.ControlPointCount; p += 3)
+        {
+            spline.SetControlPoint(p, DropPoint(randomPoint(p)));
         }
     }
 
-    void DropControlPoint(int p)
+    Vector3 randomPoint(int p)
     {
         float randomXaxis = PublicFunction.RoundUp(Random.Range(left, right), xSpacing); //The control point will only appear at a point that's a mupltable of the givin number 
-        Vector3 randomPoint = new Vector3(randomXaxis, 0, p*zSpacing);
+        return new Vector3(randomXaxis, 0, p * zSpacing);
+    }
+
+    Vector3 DropPoint(Vector3 point)
+    {      
 
         RaycastHit hit;
-        if (Physics.Raycast(randomPoint, Vector3.down, out hit, 2000)) //Cast a Raycast to see if any colliders are under that point.
+        if (Physics.Raycast(point, Vector3.down, out hit, 2000)) //Cast a Raycast to see if any colliders are under that point.
         {
-            spline.SetControlPoint(p,randomPoint + Vector3.down * hit.distance); //Drops point right on the surrface of that collider
+            return point + Vector3.down * hit.distance;            
         }
         else
         {
-            spline.SetControlPoint(p, spline.GetControlPoint(p - 1));
+            return point;
         }
     }
 }
