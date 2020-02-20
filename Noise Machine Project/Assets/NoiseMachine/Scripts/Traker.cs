76aae803e4f653f642c8d5a4782bb6f974769ded
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2020-02-20 10:54:01 -0500

    Branched off to test goals without messing with master

---
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/Traker.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traker : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float YSPos;
+    float ZSPos;
+
+    float YCPos;
+    float ZCPos;
+
+    void Start()
+    {
+        YSPos = transform.position.y;
+        ZSPos = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        YCPos = transform.position.y;
+        ZCPos = transform.position.z;
+    }
+
+    float GetDistance()
+    {
+        return YCPos;
+    }
+}
