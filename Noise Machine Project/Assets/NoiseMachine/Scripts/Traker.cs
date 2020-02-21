9e9ce5ceda48ab672d405c65a8c8f4bf1a1ddc49
Thatguyindacorner <45794181+Thatguyindacorner@users.noreply.github.com>
2020-02-21 15:30:22 -0500

    almost finished

--- Noise Machine Project/Assets/NoiseMachine/Scripts/Traker.cs
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/Traker.cs
@@ -13,12 +13,31 @@ public class Traker : MonoBehaviour
     float YCPos;
     float ZCPos;
 
-    public Text distance;
+    float YPOSroc;
+    float ZPOSroc;
+
+    float totalDis;
+    float goalDis;
+
+    float timeS;
+    Vector3 lastPoint;
+
+    float locDisStart;
+
+    public GameObject distance;
+    public GameObject UI;
+
+    public GameObject speedometer;
 
     void Start()
     {
         YSPos = transform.position.y;
         ZSPos = transform.position.z;
+        locDisStart = distance.GetComponent<RectTransform>().rect.y;
+
+        goalDis = 250;
+        timeS = Time.time;
+        lastPoint = transform.position;
     }
 
     // Update is called once per frame
@@ -27,9 +46,28 @@ public class Traker : MonoBehaviour
         YCPos = transform.position.y;
         ZCPos = transform.position.z;
 
-        distance.text = (YSPos-YCPos).ToString();
+        YPOSroc = (YSPos - YCPos);
+        ZPOSroc = (ZCPos - ZSPos);
 
-        if (YSPos - YCPos > 250)
+        if (Time.time - timeS > 1)
+        {
+
+            speedometer.GetComponent<Text>().text = (transform.position - lastPoint)*Time.deltaTime * 60 * 60).ToString() + " km/h";
+
+            timeS = Time.time;
+            lastPoint = transform.position;
+        }
+        speedometer.GetComponent<Text>().text = (totalDis*Time.deltaTime).ToString();
+
+
+        totalDis = Mathf.Sqrt((YPOSroc * YPOSroc) + (ZPOSroc * ZPOSroc));
+
+        DistanceTravelled();
+
+        distance.GetComponent<Text>().text = (totalDis).ToString();
+        //distance.text = (ZPOSroc).ToString();
+
+        if (totalDis > goalDis)
         {
             print("You Win");
         }
@@ -38,5 +76,12 @@ public class Traker : MonoBehaviour
     float GetDistance()
     {
         return YCPos;
+    }
+
+    void DistanceTravelled()
+    {
+        //Shows the elevation of the player moving towards the goal by moving the text displaying the elevation from the start until the goal (happens in the position.y)
+        distance.GetComponent<RectTransform>().position = new Vector3(distance.GetComponent<RectTransform>().position.x, 500 - ( 500 * (totalDis / goalDis)), distance.GetComponent<RectTransform>().position.z);
+
     }
 }
