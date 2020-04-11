fcb10556dcbd67581bdd9379d91a6fd037e8d0dd
Shane <48694586+ShaneLogtenberg@users.noreply.github.com>
2020-04-10 22:28:35 -0400

    New CanvasManager

---
+++ Noise Machine Project/Assets/NoiseMachine/Scripts/CanvasManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasManager : MonoBehaviour
+{
+    //UI references
+    public GameObject distanceDisplay;
+    public GameObject winText;
+    public GameObject speedometer;
+
+    public GameObject PauseMenu;
+    public GameObject dTravel;
+    public GameObject mSpeed;
+    public GameObject flagScoreText;
+
+    public GameObject PauseButton;
+    public Text PauseText;
+
+    public GameObject Trophy;
+}
